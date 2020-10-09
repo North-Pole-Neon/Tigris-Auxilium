e63b3aaa8512bb118a3448e1fdd9ec9e2d62e12b
Neopect <tyler.moen@earthlink.net>
2020-10-09 15:48:31 -0500

    Testing out

---
+++ src/main/java/main/Controllers/MainCon.java
@@ -0,0 +1,13 @@
+package main.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class MainCon {
+    public Label lbl;
+    public int clicks;
+    public void onNewBtn(ActionEvent actionEvent) {
+        clicks++;
+        lbl.setText("You clicked " + clicks + " times");
+    }
+}
